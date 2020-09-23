10e81faf866e7f2f7633e24d3cbca8f27e89336a
Z Xian <alex.xian@hotmail.com>
2020-09-23 03:43:36 +0000

    made it longer

--- spinner1.js
+++ spinner1.js
@@ -14,3 +14,42 @@ setTimeout(() => {
   // Need to escape the backslash since it's a special character.
   process.stdout.write('\r\\   '); 
 }, 700);
+
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 1500);
+
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1700);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1900);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 2200);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+  console.log("\n")
+}, 2400);
+
+
